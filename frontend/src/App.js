f900af550fb7986abab7beb5225ae833cd244b68
Maksym Pavliv <maksiopavliv0@gmail.com>
2023-06-28 16:50:53 +0300

    add header and dummy product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footter from "./components/Footter";
+import HomeScreen from "./screens/HomeScreen";
+
+
 const App = () => {
   return (
-    <h1>Welcome to pro shop</h1>
-  )
+    <>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footter/>
+    </>
+  );
 }
 
 export default App
