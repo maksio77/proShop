bd7a0041e86122e1573d95002e1c39bad05dd000
Maksym Pavliv <maksiopavliv0@gmail.com>
2023-06-29 12:08:37 +0300

    merge backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footter from "./components/Footter";
-import HomeScreen from "./screens/HomeScreen";
 
 
 const App = () => {
@@ -10,7 +11,7 @@ const App = () => {
       <Header/>
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Outlet/>
         </Container>
       </main>
       <Footter/>
